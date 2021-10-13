25faa38d38e7372ec55bb429e69ca1cea304d1b3
Hector Castellanos <hectorcaste94@gmail.com>
2021-10-13 14:33:33 -0600

    first commit; '

---
+++ main.js
@@ -0,0 +1,43 @@
+$(document).ready(()=>{
+
+ const $content =$ ('.jumbo1');
+ $($content).mouseover(function(){
+    
+    $content.css("background","Grey");
+    
+    });
+    $($content).mouseout(function(){
+    
+        $content.css("background","White");
+        
+        });
+        
+        const $content1 =$ ('.jumbo2');
+        $($content1).mouseover(function(){
+           
+           $content1.css("background","Grey");
+           
+           });
+           $($content1).mouseout(function(){
+           
+               $content1.css("background","White");
+               
+               });
+               
+               const $content3 =$ ('.jumbo3');
+               $($content3).mouseover(function(){
+                  
+                  $content3.css("background","Grey");
+                  
+                  });
+                  $($content3).mouseout(function(){
+                  
+                      $content3.css("background","White");
+                      
+                      });
+                      
+
+
+
+    
+        });
