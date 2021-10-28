9d548c7fdbd8f8ecd272264c64d81166c766ce4d
Hector Castellanos <hectorcaste94@gmail.com>
2021-10-27 19:19:10 -0600

    all work

--- main.js
+++ main.js
@@ -1,43 +1,61 @@
-$(document).ready(()=>{
+$(document).ready(() => {
 
- const $content =$ ('.jumbo1');
- $($content).mouseover(function(){
-    
-    $content.css("background","Grey");
-    
+
+    const $contactcontent = $('.forcontact');
+    const $formcontent = $('.formcontainer');
+    $formcontent.hide();
+    $contactcontent.hide();
+    const $contactcontentbutton = $('.contactbutton');
+    const $signbutton = $('.signbutton')
+
+    $($signbutton).on('click', () => {
+        $formcontent.toggle();
     });
-    $($content).mouseout(function(){
-    
-        $content.css("background","White");
-        
-        });
-        
-        const $content1 =$ ('.jumbo2');
-        $($content1).mouseover(function(){
-           
-           $content1.css("background","Grey");
-           
-           });
-           $($content1).mouseout(function(){
-           
-               $content1.css("background","White");
-               
-               });
-               
-               const $content3 =$ ('.jumbo3');
-               $($content3).mouseover(function(){
-                  
-                  $content3.css("background","Grey");
-                  
-                  });
-                  $($content3).mouseout(function(){
-                  
-                      $content3.css("background","White");
-                      
-                      });
-                      
+
+
+    $($contactcontentbutton).on('click', () => {
+        $contactcontent.toggle();
+    });
+
+
+    const $content = $('.jumbo1');
+    $($content).mouseover(function() {
+
+        $content.css("background", "Grey");
+
+    });
+    $($content).mouseout(function() {
+
+        $content.css("background", "White");
+
+    });
+
+    const $content1 = $('.jumbo2');
+    $($content1).mouseover(function() {
+
+        $content1.css("background", "Grey");
+
+    });
+    $($content1).mouseout(function() {
+
+        $content1.css("background", "White");
+
+    });
+
+    const $content3 = $('.jumbo3');
+    $($content3).mouseover(function() {
+
+        $content3.css("background", "Grey");
+
+    });
+    $($content3).mouseout(function() {
+
+        $content3.css("background", "White");
+
+    });
 
 
 
-    
-        });
+
+
+});
